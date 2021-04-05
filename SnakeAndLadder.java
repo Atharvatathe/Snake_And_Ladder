ba1318b350c296178d306659f4c97d9432ce125b
Atharva <atharva.tathe55@gmail.com>
2021-04-05 16:27:57 +0530

    Adding: Check option between noPlay,Ladde or Snake

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,10 +5,41 @@ public class SnakeAndLadder
 	{
 
 		int start = 0;
+		int won = 0;
 		int player1 = start;
 
-		int DieNo = (int) Math.floor(Math.random() *6+1 );
+		int Noplay = 0;
+		int Ladder = 1;
+		int Snake = 2;
 
-		System.out.println("Die number : " +DieNo);
+		while( won < 100 )
+		{
+			
+			int DieNo = (int) Math.floor(Math.random() *6+1 );
+			int option = (int) Math.floor(Math.random() * 10) % 3 ;
+
+			System.out.println("Die number : " +DieNo);
+			System.out.println("option : " +option);
+
+
+			if(option == Noplay)
+			{
+				System.out.println("Player1 is not played");
+			}
+			else if(option == Ladder)
+			{
+				System.out.println("Player Got Ladder");
+				player1 = player1 + DieNo ;
+			}
+			else
+			{
+				System.out.println("Player Got Snake");
+				player1 = player1 - DieNo ;
+			}
+			won = player1;
+			System.out.println("Won: " +won);
+			System.out.println("Player1 position is " +player1);
+			System.out.println("\n");
+		}
 	}
 }
