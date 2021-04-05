aa3b7d65e4cff5dfe68f616677d403670bfd3a49
Atharva <atharva.tathe55@gmail.com>
2021-04-05 16:34:58 +0530

    Adding condition : if player position goes above 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,20 +1,20 @@
 public class SnakeAndLadder
 {
 
-public static void main(String[] args)
+
+	public static void main(String[] args)
 	{
 
 		int start = 0;
 		int won = 0;
 		int player1 = start;
-
 		int Noplay = 0;
 		int Ladder = 1;
 		int Snake = 2;
 
 		while( won < 100 )
 		{
-			
+
 			int DieNo = (int) Math.floor(Math.random() *6+1 );
 			int option = (int) Math.floor(Math.random() * 10) % 3 ;
 
@@ -24,7 +24,7 @@ public static void main(String[] args)
 			if(player1 < 0 )
 				player1 = 0;
 
-
+			
 			if(option == Noplay)
 			{
 				System.out.println("Player1 is not played");
@@ -39,10 +39,19 @@ public static void main(String[] args)
 				System.out.println("Player Got Snake");
 				player1 = player1 - DieNo ;
 			}
+
+
+			if(player1 > 100)
+				player1=won;
+
 			won = player1;
+
+
 			System.out.println("Won: " +won);
 			System.out.println("Player1 position is " +player1);
 			System.out.println("\n");
 		}
+
+		System.out.println("Player1 Won the game");
 	}
 }
